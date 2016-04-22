5e63117dc8377029aeeeee82b03825552cd36fa6
MatthiasL <matthias_ludwig@yahoo.com>
2016-04-23 05:35:00 +1000

    17 Images Timer

---
+++ public/17_Images_Timer/program.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+  setAllImagesBlank();
+  $('img').click(onImgClick);
+});
+
+
+var setAllImagesBlank = function () {
+  $('#diceImg1').attr('src', 'images/blank.jpg');
+  $('#diceImg2').attr('src', 'images/blank.jpg');
+  $('#diceImg3').attr('src', 'images/blank.jpg');
+};
+
+
+var onImgClick = function (e) {
+  $target = $(e.currentTarget);
+  var id = $target.attr('id');
+
+  var imageIdNumber = id.substring(id.length - 1);
+
+  console.log('imageIdNumber = ', imageIdNumber);
+
+  $target.attr('src', 'images/' + imageIdNumber + '.jpg');
+  startTimer();
+};
+
+var timer;
+
+function startTimer() {
+  clearInterval(timer);
+
+  timer = setInterval(function () {
+    setAllImagesBlank();
+  }, 5000);
+}
