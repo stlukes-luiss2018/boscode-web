b410982ba19526f44707eba3015dc92db3c90c64
MatthiasL <matthias_ludwig@yahoo.com>
2016-04-23 05:15:10 +1000

    ## 16 - Images

---
+++ public/16_Images/program.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+  setAllImagesBlank();
+  $('img').click(onImgClick);
+});
+
+
+var setAllImagesBlank = function () {
+  $('#diceImg1').attr('src', 'images/blank.jpg');
+  $('#diceImg2').attr('src', 'images/blank.jpg');
+  $('#diceImg3').attr('src', 'images/blank.jpg');
+};
+
+
+var onImgClick = function (e) {
+  $target = $(e.currentTarget);
+  var id = $target.attr('id');
+
+  var imageIdNumber = id.substring(id.length - 1);
+
+  console.log('imageIdNumber = ', imageIdNumber);
+
+  $target.attr('src', 'images/' + imageIdNumber + '.jpg');
+};
+
